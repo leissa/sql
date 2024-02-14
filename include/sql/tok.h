9825049f4601c1eb03c5d1cb4c67b1d3220043ca
Roland Leißa <leissa@uni-mannheim.de>
2024-02-14 14:19:11 +0100

    split keywords into keys/non-keys

--- include/sql/tok.h
+++ include/sql/tok.h
@@ -12,7 +12,37 @@ using fe::Loc;
 using fe::Pos;
 using fe::Sym;
 
+#define SQL_TOK(m)                              \
+    m(EoF,          "<end of file>")            \
+    /* value: contains sth beyond the tag */    \
+    m(V_int,        "<interger value>")         \
+    m(V_id,         "<identifier>")             \
+    /* delimiter */                             \
+    m(D_brace_l,    "{")                        \
+    m(D_brace_r,    "}")                        \
+    m(D_brckt_l,    "[")                        \
+    m(D_brckt_r,    "]")                        \
+    m(D_paren_l,    "(")                        \
+    m(D_paren_r,    ")")                        \
+    /* further tokens */                        \
+    m(T_assign,     ":=")                       \
+    m(T_eq,         "=")                        \
+    m(T_ne,         "<>")                       \
+    m(T_l,          "<")                        \
+    m(T_g,          ">")                        \
+    m(T_le,         "<=")                       \
+    m(T_ge,         ">=")                       \
+    m(T_dot,        ".")                        \
+    m(T_colon,      ":")                        \
+    m(T_comma,      ",")                        \
+    m(T_semicolon,  ";")                        \
+    m(T_add,        "+")                        \
+    m(T_sub,        "-")                        \
+    m(T_mul,        "*")                        \
+    m(T_div,        "/")                        \
+
 // clang-format off
+// These are the *real* keywords - "reserved words"
 #define SQL_KEY(m) \
     m(K_ABS, "ABS") \
     m(K_ALL, "ALL") \
@@ -337,39 +367,239 @@ using fe::Sym;
     m(K_WITH, "WITH") \
     m(K_WITHIN, "WITHIN") \
     m(K_WITHOUT, "WITHOUT") \
-    m(K_YEAR, "YEAR") \
+    m(K_YEAR, "YEAR")
 
-#define SQL_TOK(m)                              \
-    m(EoF,          "<end of file>")            \
-    /* value: contains sth beyond the tag */    \
-    m(V_int,        "<interger value>")         \
-    m(V_id,         "<identifier>")             \
-    /* delimiter */                             \
-    m(D_brace_l,    "{")                        \
-    m(D_brace_r,    "}")                        \
-    m(D_brckt_l,    "[")                        \
-    m(D_brckt_r,    "]")                        \
-    m(D_paren_l,    "(")                        \
-    m(D_paren_r,    ")")                        \
-    /* further tokens */                        \
-    m(T_assign,     ":=")                       \
-    m(T_eq,         "=")                        \
-    m(T_ne,         "<>")                       \
-    m(T_l,          "<")                        \
-    m(T_g,          ">")                        \
-    m(T_le,         "<=")                       \
-    m(T_ge,         ">=")                       \
-    m(T_dot,        ".")                        \
-    m(T_colon,      ":")                        \
-    m(T_comma,      ",")                        \
-    m(T_semicolon,  ";")                        \
-    m(T_add,        "+")                        \
-    m(T_sub,        "-")                        \
-    m(T_mul,        "*")                        \
-    m(T_div,        "/")                        \
+// These are *non-reserved words* that only have a special meaning in particular contexts and can be used as identifiers in other contexts.
+#define SQL_NON_KEY(m) \
+    m(N_A, "A") \
+    m(N_ABSOLUTE, "ABSOLUTE") \
+    m(N_ACTION, "ACTION") \
+    m(N_ADA, "ADA") \
+    m(N_ADD, "ADD") \
+    m(N_ADMIN, "ADMIN") \
+    m(N_AFTER, "AFTER") \
+    m(N_ALWAYS, "ALWAYS") \
+    m(N_ASC, "ASC") \
+    m(N_ASSERTION, "ASSERTION") \
+    m(N_ASSIGNMENT, "ASSIGNMENT") \
+    m(N_ATTRIBUTE, "ATTRIBUTE") \
+    m(N_ATTRIBUTES, "ATTRIBUTES") \
+    m(N_BEFORE, "BEFORE") \
+    m(N_BERNOULLI, "BERNOULLI") \
+    m(N_BREADTH, "BREADTH") \
+    m(N_C, "C") \
+    m(N_CASCADE, "CASCADE") \
+    m(N_CATALOG, "CATALOG") \
+    m(N_CATALOG_NAME, "CATALOG_NAME") \
+    m(N_CHAIN, "CHAIN") \
+    m(N_CHARACTER_SET_CATALOG, "CHARACTER_SET_CATALOG") \
+    m(N_CHARACTER_SET_NAME, "CHARACTER_SET_NAME") \
+    m(N_CHARACTER_SET_SCHEMA, "CHARACTER_SET_SCHEMA") \
+    m(N_CHARACTERISTICS, "CHARACTERISTICS") \
+    m(N_CHARACTERS, "CHARACTERS") \
+    m(N_CLASS_ORIGIN, "CLASS_ORIGIN") \
+    m(N_COBOL, "COBOL") \
+    m(N_COLLATION, "COLLATION") \
+    m(N_COLLATION_CATALOG, "COLLATION_CATALOG") \
+    m(N_COLLATION_NAME, "COLLATION_NAME") \
+    m(N_COLLATION_SCHEMA, "COLLATION_SCHEMA") \
+    m(N_COLUMN_NAME, "COLUMN_NAME") \
+    m(N_COMMAND_FUNCTION, "COMMAND_FUNCTION") \
+    m(N_COMMAND_FUNCTION_CODE, "COMMAND_FUNCTION_CODE") \
+    m(N_COMMITTED, "COMMITTED") \
+    m(N_CONDITION_NUMBER, "CONDITION_NUMBER") \
+    m(N_CONNECTION, "CONNECTION") \
+    m(N_CONNECTION_NAME, "CONNECTION_NAME") \
+    m(N_CONSTRAINT_CATALOG, "CONSTRAINT_CATALOG") \
+    m(N_CONSTRAINT_NAME, "CONSTRAINT_NAME") \
+    m(N_CONSTRAINT_SCHEMA, "CONSTRAINT_SCHEMA") \
+    m(N_CONSTRAINTS, "CONSTRAINTS") \
+    m(N_CONSTRUCTOR, "CONSTRUCTOR") \
+    m(N_CONTINUE, "CONTINUE") \
+    m(N_CURSOR_NAME, "CURSOR_NAME") \
+    m(N_DATA, "DATA") \
+    m(N_DATETIME_INTERVAL_CODE, "DATETIME_INTERVAL_CODE") \
+    m(N_DATETIME_INTERVAL_PRECISION, "DATETIME_INTERVAL_PRECISION") \
+    m(N_DEFAULTS, "DEFAULTS") \
+    m(N_DEFERRABLE, "DEFERRABLE") \
+    m(N_DEFERRED, "DEFERRED") \
+    m(N_DEFINED, "DEFINED") \
+    m(N_DEFINER, "DEFINER") \
+    m(N_DEGREE, "DEGREE") \
+    m(N_DEPTH, "DEPTH") \
+    m(N_DERIVED, "DERIVED") \
+    m(N_DESC, "DESC") \
+    m(N_DESCRIPTOR, "DESCRIPTOR") \
+    m(N_DIAGNOSTICS, "DIAGNOSTICS") \
+    m(N_DISPATCH, "DISPATCH") \
+    m(N_DOMAIN, "DOMAIN") \
+    m(N_DYNAMIC_FUNCTION, "DYNAMIC_FUNCTION") \
+    m(N_DYNAMIC_FUNCTION_CODE, "DYNAMIC_FUNCTION_CODE") \
+    m(N_ENFORCED, "ENFORCED") \
+    m(N_EXCLUDE, "EXCLUDE") \
+    m(N_EXCLUDING, "EXCLUDING") \
+    m(N_EXPRESSION, "EXPRESSION") \
+    m(N_FINAL, "FINAL") \
+    m(N_FIRST, "FIRST") \
+    m(N_FLAG, "FLAG") \
+    m(N_FOLLOWING, "FOLLOWING") \
+    m(N_FORTRAN, "FORTRAN") \
+    m(N_FOUND, "FOUND") \
+    m(N_G, "G") \
+    m(N_GENERAL, "GENERAL") \
+    m(N_GENERATED, "GENERATED") \
+    m(N_GO, "GO") \
+    m(N_GOTO, "GOTO") \
+    m(N_GRANTED, "GRANTED") \
+    m(N_HIERARCHY, "HIERARCHY") \
+    m(N_IGNORE, "IGNORE") \
+    m(N_IMMEDIATE, "IMMEDIATE") \
+    m(N_IMMEDIATELY, "IMMEDIATELY") \
+    m(N_IMPLEMENTATION, "IMPLEMENTATION") \
+    m(N_INCLUDING, "INCLUDING") \
+    m(N_INCREMENT, "INCREMENT") \
+    m(N_INITIALLY, "INITIALLY") \
+    m(N_INPUT, "INPUT") \
+    m(N_INSTANCE, "INSTANCE") \
+    m(N_INSTANTIABLE, "INSTANTIABLE") \
+    m(N_INSTEAD, "INSTEAD") \
+    m(N_INVOKER, "INVOKER") \
+    m(N_ISOLATION, "ISOLATION") \
+    m(N_K, "K") \
+    m(N_KEY, "KEY") \
+    m(N_KEY_MEMBER, "KEY_MEMBER") \
+    m(N_KEY_TYPE, "KEY_TYPE") \
+    m(N_LAST, "LAST") \
+    m(N_LENGTH, "LENGTH") \
+    m(N_LEVEL, "LEVEL") \
+    m(N_LOCATOR, "LOCATOR") \
+    m(N_M, "M") \
+    m(N_MAP, "MAP") \
+    m(N_MATCHED, "MATCHED") \
+    m(N_MAXVALUE, "MAXVALUE") \
+    m(N_MESSAGE_LENGTH, "MESSAGE_LENGTH") \
+    m(N_MESSAGE_OCTET_LENGTH, "MESSAGE_OCTET_LENGTH") \
+    m(N_MESSAGE_TEXT, "MESSAGE_TEXT") \
+    m(N_MINVALUE, "MINVALUE") \
+    m(N_MORE, "MORE") \
+    m(N_MUMPS, "MUMPS") \
+    m(N_NAME, "NAME") \
+    m(N_NAMES, "NAMES") \
+    m(N_NESTING, "NESTING") \
+    m(N_NEXT, "NEXT") \
+    m(N_NFC, "NFC") \
+    m(N_NFD, "NFD") \
+    m(N_NFKC, "NFKC") \
+    m(N_NFKD, "NFKD") \
+    m(N_NORMALIZED, "NORMALIZED") \
+    m(N_NULLABLE, "NULLABLE") \
+    m(N_NULLS, "NULLS") \
+    m(N_NUMBER, "NUMBER") \
+    m(N_OBJECT, "OBJECT") \
+    m(N_OCTETS, "OCTETS") \
+    m(N_OPTION, "OPTION") \
+    m(N_OPTIONS, "OPTIONS") \
+    m(N_ORDERING, "ORDERING") \
+    m(N_ORDINALITY, "ORDINALITY") \
+    m(N_OTHERS, "OTHERS") \
+    m(N_OUTPUT, "OUTPUT") \
+    m(N_OVERRIDING, "OVERRIDING") \
+    m(N_P, "P") \
+    m(N_PAD, "PAD") \
+    m(N_PARAMETER_MODE, "PARAMETER_MODE") \
+    m(N_PARAMETER_NAME, "PARAMETER_NAME") \
+    m(N_PARAMETER_ORDINAL_POSITION, "PARAMETER_ORDINAL_POSITION") \
+    m(N_PARAMETER_SPECIFIC_CATALOG, "PARAMETER_SPECIFIC_CATALOG") \
+    m(N_PARAMETER_SPECIFIC_NAME, "PARAMETER_SPECIFIC_NAME") \
+    m(N_PARAMETER_SPECIFIC_SCHEMA, "PARAMETER_SPECIFIC_SCHEMA") \
+    m(N_PARTIAL, "PARTIAL") \
+    m(N_PASCAL, "PASCAL") \
+    m(N_PATH, "PATH") \
+    m(N_PLACING, "PLACING") \
+    m(N_PLI, "PLI") \
+    m(N_PRECEDING, "PRECEDING") \
+    m(N_PRESERVE, "PRESERVE") \
+    m(N_PRIOR, "PRIOR") \
+    m(N_PRIVILEGES, "PRIVILEGES") \
+    m(N_PUBLIC, "PUBLIC") \
+    m(N_READ, "READ") \
+    m(N_RELATIVE, "RELATIVE") \
+    m(N_REPEATABLE, "REPEATABLE") \
+    m(N_RESPECT, "RESPECT") \
+    m(N_RESTART, "RESTART") \
+    m(N_RESTRICT, "RESTRICT") \
+    m(N_RETURNED_CARDINALITY, "RETURNED_CARDINALITY") \
+    m(N_RETURNED_LENGTH, "RETURNED_LENGTH") \
+    m(N_RETURNED_OCTET_LENGTH, "RETURNED_OCTET_LENGTH") \
+    m(N_RETURNED_SQLSTATE, "RETURNED_SQLSTATE") \
+    m(N_ROLE, "ROLE") \
+    m(N_ROUTINE, "ROUTINE") \
+    m(N_ROUTINE_CATALOG, "ROUTINE_CATALOG") \
+    m(N_ROUTINE_NAME, "ROUTINE_NAME") \
+    m(N_ROUTINE_SCHEMA, "ROUTINE_SCHEMA") \
+    m(N_ROW_COUNT, "ROW_COUNT") \
+    m(N_SCALE, "SCALE") \
+    m(N_SCHEMA, "SCHEMA") \
+    m(N_SCHEMA_NAME, "SCHEMA_NAME") \
+    m(N_SCOPE_CATALOG, "SCOPE_CATALOG") \
+    m(N_SCOPE_NAME, "SCOPE_NAME") \
+    m(N_SCOPE_SCHEMA, "SCOPE_SCHEMA") \
+    m(N_SECTION, "SECTION") \
+    m(N_SECURITY, "SECURITY") \
+    m(N_SELF, "SELF") \
+    m(N_SEQUENCE, "SEQUENCE") \
+    m(N_SERIALIZABLE, "SERIALIZABLE") \
+    m(N_SERVER_NAME, "SERVER_NAME") \
+    m(N_SESSION, "SESSION") \
+    m(N_SETS, "SETS") \
+    m(N_SIMPLE, "SIMPLE") \
+    m(N_SIZE, "SIZE") \
+    m(N_SOURCE, "SOURCE") \
+    m(N_SPACE, "SPACE") \
+    m(N_SPECIFIC_NAME, "SPECIFIC_NAME") \
+    m(N_STATE, "STATE") \
+    m(N_STATEMENT, "STATEMENT") \
+    m(N_STRUCTURE, "STRUCTURE") \
+    m(N_STYLE, "STYLE") \
+    m(N_SUBCLASS_ORIGIN, "SUBCLASS_ORIGIN") \
+    m(N_T, "T") \
+    m(N_TABLE_NAME, "TABLE_NAME") \
+    m(N_TEMPORARY, "TEMPORARY") \
+    m(N_TIES, "TIES") \
+    m(N_TOP_LEVEL_COUNT, "TOP_LEVEL_COUNT") \
+    m(N_TRANSACTION, "TRANSACTION") \
+    m(N_TRANSACTION_ACTIVE, "TRANSACTION_ACTIVE") \
+    m(N_TRANSACTIONS_COMMITTED, "TRANSACTIONS_COMMITTED") \
+    m(N_TRANSACTIONS_ROLLED_BACK, "TRANSACTIONS_ROLLED_BACK") \
+    m(N_TRANSFORM, "TRANSFORM") \
+    m(N_TRANSFORMS, "TRANSFORMS") \
+    m(N_TRIGGER_CATALOG, "TRIGGER_CATALOG") \
+    m(N_TRIGGER_NAME, "TRIGGER_NAME") \
+    m(N_TRIGGER_SCHEMA, "TRIGGER_SCHEMA") \
+    m(N_TYPE, "TYPE") \
+    m(N_UNBOUNDED, "UNBOUNDED") \
+    m(N_UNCOMMITTED, "UNCOMMITTED") \
+    m(N_UNDER, "UNDER") \
+    m(N_UNNAMED, "UNNAMED") \
+    m(N_USAGE, "USAGE") \
+    m(N_USER_DEFINED_TYPE_CATALOG, "USER_DEFINED_TYPE_CATALOG") \
+    m(N_USER_DEFINED_TYPE_CODE, "USER_DEFINED_TYPE_CODE") \
+    m(N_USER_DEFINED_TYPE_NAME, "USER_DEFINED_TYPE_NAME") \
+    m(N_USER_DEFINED_TYPE_SCHEMA, "USER_DEFINED_TYPE_SCHEMA") \
+    m(N_VIEW, "VIEW") \
+    m(N_WORK, "WORK") \
+    m(N_WRITE, "WRITE") \
+    m(N_ZONE, "ZONE")
+
+enum class NonKey {
+#define CODE(t, _) t,
+    SQL_NON_KEY(CODE)
+#undef CODE
+};
 
 #define CODE(t, str) + 1
 constexpr auto Num_Keys = 0 SQL_KEY(CODE);
+constexpr auto Num_Non_Keys = 0 SQL_NON_KEY(CODE);
 #undef CODE
 
 class Tok {
