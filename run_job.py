c62a93593638e468d91e3357ebe75d4918088d8e
Kirill <kirill.gratchev@uni-mannheim.de>
2024-07-23 12:52:35 +0200

    run_job now tests all job files

--- run_job.py
+++ run_job.py
@@ -1,7 +1,25 @@
 import subprocess
 import os
+import glob
 
 print(os.getcwd())
 
-tmp = subprocess.call("./build/bin/sql -d ./test/select.sql", shell=True)
-print(tmp)
+
+job_files = glob.glob("./job/*.sql")
+
+successes = 0
+failures = 0
+segfaults = 0
+
+for file in job_files:
+    tmp = subprocess.call(f'./build/bin/sql -d {file}', shell=True)
+    print(tmp)
+    if tmp == 0:
+        successes += 1
+    elif tmp == 1:
+        failures += 1
+    elif tmp == 139:
+        segfaults += 1
+
+print("Job's done")
+print(f'Successes: {successes} Failures: {failures} Segfaults: {segfaults}')
